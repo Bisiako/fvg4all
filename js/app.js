224bc6f0d935eccf349ae90c5f9d7da3c30318c9
Bisiako <80096184+Bisiako@users.noreply.github.com>
2026-09-10 18:30:24 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -264,22 +264,13 @@ async function findImages() {
             .substring(root.length + 1)
             .split("/")[0],
 
-   /*     url:
+       url:
           `https://raw.githubusercontent.com/` +
           `${CONFIG.OWNER}/` +
           `${CONFIG.REPO}/` +
           `${CONFIG.BRANCH}/` +
-          item.path */
+          item.path 
 
-url:
-  `https://raw.githubusercontent.com/` +
-  `${CONFIG.OWNER}/` +
-  `${CONFIG.REPO}/` +
-  `${CONFIG.BRANCH}/` +
-  item.path
-    .split("/")
-    .map(encodeURIComponent)
-    .join("/")
 
         
       };
